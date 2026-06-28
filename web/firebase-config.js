7b7fab43df16fe6b4f75f8ce5df701ea1ee9e9e1
pakho2433 <pakho2433@gmail.com>
2026-06-28 18:12:30 +0800

    Sync Firebase web configuration

--- web/firebase-config.js
+++ web/firebase-config.js
@@ -1,16 +1,11 @@
-const apiKey = [
-  'AIzaSyBrtUm0z',
-  'QzjUxWqx40_',
-  'FaMdblhlcoeXPmw',
-].join('');
-
 export const firebaseConfig = {
-  apiKey,
+  apiKey: 'AIzaSyBrtUmOzQzjUxWqx40_FaMdblhlcoeXPmw',
   authDomain: 'couple-app-852c2.firebaseapp.com',
   projectId: 'couple-app-852c2',
   storageBucket: 'couple-app-852c2.firebasestorage.app',
   messagingSenderId: '112711337609',
   appId: '1:112711337609:web:c4425295b0da1a62dae858',
+  measurementId: 'G-L4RPZ977TQ',
 };
 
-export const publicAppUrl = 'https://pakho2433.github.io/couple-app/';
+export const publicAppUrl = 'https://pakho2433.github.io/couple-app/web/';
