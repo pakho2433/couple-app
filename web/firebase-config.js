d61fe19fe5febcd53f89966b1a84594309f0d3d0
pakho2433 <pakho2433@gmail.com>
2026-06-28 16:33:04 +0800

    Add deploy-time Firebase config template

---
+++ web/firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: '__FIREBASE_API_KEY__',
+  authDomain: '__FIREBASE_AUTH_DOMAIN__',
+  projectId: '__FIREBASE_PROJECT_ID__',
+  storageBucket: '__FIREBASE_STORAGE_BUCKET__',
+  messagingSenderId: '__FIREBASE_MESSAGING_SENDER_ID__',
+  appId: '__FIREBASE_APP_ID__',
+};
+
+export const vapidKey = '__FIREBASE_VAPID_KEY__';
+export const publicAppUrl = 'https://pakho2433.github.io/couple-app/';
