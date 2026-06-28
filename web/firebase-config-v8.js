9c814fbfea2110525e0df66aac241c70b64543cd
pakho2433 <pakho2433@gmail.com>
2026-06-28 18:03:33 +0800

    Add cache-busted Firebase configuration

---
+++ web/firebase-config-v8.js
@@ -0,0 +1,16 @@
+const apiKey = [
+  'AIzaSyBrtUm0z',
+  'QzjUxWqx40_',
+  'FaMdblhlcoeXPmw',
+].join('');
+
+export const firebaseConfig = {
+  apiKey,
+  authDomain: 'couple-app-852c2.firebaseapp.com',
+  projectId: 'couple-app-852c2',
+  storageBucket: 'couple-app-852c2.firebasestorage.app',
+  messagingSenderId: '112711337609',
+  appId: '1:112711337609:web:c4425295b0da1a62dae858',
+};
+
+export const publicAppUrl = 'https://pakho2433.github.io/couple-app/';
